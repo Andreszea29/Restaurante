c3120cfd491a1cce7ec2ea9cd467d20d6e16cbbc
Andrés <zeaastream9@gmail.com>
2026-06-02 15:50:48 -0500

    restaurante

---
+++ restaurante/javascript/pedidos.js
@@ -0,0 +1,3 @@
+let metodo_pago =  document.getElementById("metodo_pago").value;
+let descripcion = document.getElementById("descripcion").value;
+let referencias = document.getElementById("referencias").value; 
---
+++ restaurante/javascript/usuarios.js
@@ -0,0 +1,7 @@
+let nombre = document.getElementById("nombre").value;
+let email = document.getElementById("email").value;
+let celular = document.getElementById("celular").value;
+let genero = document.getElementById("genero").value;
+let fecha_nacimiento = document.getElementById("fecha_nacimiento").value;
+let direccion = document.getElementById("direccion").value;
+let contrasena = document.getElementById("contrasena").value;
