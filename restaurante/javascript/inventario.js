460cdea03cdeacc8dffabd8aab69d7b4e9ffc5b3
Andrés <zeaastream9@gmail.com>
2026-06-09 17:39:08 -0500

    Validate_user_infomration : validate information

--- restaurante/javascript/inventario.js
+++ restaurante/javascript/inventario.js
@@ -5,3 +5,32 @@ let cantidad_producto = document.getElementById("cantidad_producto").value;
 let marca_producto = document.getElementById("marca_producto").value;
 let categoria_producto = document.getElementById("categoria_producto").value;
 let unidad_medida = document.getElementById("unidad_medida").value;
+
+function validarDatos() {
+
+    // letras 
+    if (codigo_producto != "" || precio_unitario != "" || nombre_producto != "" || cantidad_producto != "" || marca_producto != "" || categoria_producto != "" || unidad_medida != "" ) {
+        if(codigo_producto == /[a-zA-Z]/){
+            console.log("Los datos ingresados son incorrectos")
+        }
+        if (precio_unitario == /[a-zA-Z]/){
+            console.log("Los datos ingresados son incorrectos")
+        }
+        if (cantidad_producto == /[a-zA-Z]/){
+            console.log("Los datos ingresados son incorrectos")
+        }
+    //Números
+        if (nombre_producto ==  /[^\d+$]/ ){
+            console.log("Los datos ingresados son incorrectos") 
+        }
+        if (unidad_medida == /[^\d+$]/ ){
+            console.log("Los datos ingresados son incorrectos")
+        }
+        if (categoria_producto == /[^\d+$]/){
+            console.log("Los datos ingresados son incorrectos")
+        }
+    }
+    else{
+        console.log("Ingrese los datos correctos")
+    }
+}
--- restaurante/javascript/pedidos.js
+++ restaurante/javascript/pedidos.js
@@ -1,3 +1,28 @@
 let metodo_pago =  document.getElementById("metodo_pago").value;
 let descripcion = document.getElementById("descripcion").value;
 let referencias = document.getElementById("referencias").value; 
+let direccion = document.getElementById("direccion").value;
+let nombre = document.getElementById("nombre").value;
+let email = document.getElementById("email").value;
+let telefono = document.getElementById("telefono").value;
+let pedido = document.getElementById("pedido").value;
+
+
+function validarDatos() {
+
+    // letras 
+    if (metodo_pago != "" || descripcion != "" || referencias != "" || direccion != "" || nombre != "" || email != "" || telefono != "" || pedido != "" ){
+
+        if (nombres ==  /[^\d+$]/ ){
+            console.log("Los datos ingresados son incorrectos")
+        }
+        if (email == /[^\d+$]/){
+            console.log("Los datos ingresados son incorrectos")
+        }
+    }
+    else{
+        console.log("Ingrese los datos correctos")
+    }
+}
+
+ // Nombre (que no contenga números), Email(que contenga @), telefono (Que no contenga letras y que no sea mayor  10 digitos)
